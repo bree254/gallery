69f2d6f14b335ef842d802d5170bb6dcb9ea43e5
bree254 <brendawanjiru72@gmail.com>
2025-09-18 17:02:15 +0300

    Configure MongoDB Atlas connection and update dependencies

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://breezy_db_user:root@gallery-app-cluster.kt98dgw.mongodb.net/darkroom?retryWrites=true&w=majority&appName=gallery-app-cluster',
+    development: 'mongodb+srv://breezy_db_user:root@gallery-app-cluster.kt98dgw.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=gallery-app-cluster',
+    test: 'mongodb+srv://breezy_db_user:root@gallery-app-cluster.kt98dgw.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=gallery-app-cluster',
 }
 module.exports = config;
