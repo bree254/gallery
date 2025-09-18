a0124f8d98270958b867a9c8ee44cbd37c5625ce
bree254 <brendawanjiru72@gmail.com>
2025-09-18 17:18:48 +0300

    Implement secure environment variable configurations for MongoDB credentials

--- _config.js
+++ _config.js
@@ -1,9 +1,15 @@
+require('dotenv').config();
+
 var config = {}
 
-// Update to have your correct username and password
+// MongoDB configuration using environment variables
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const cluster = process.env.MONGO_CLUSTER;
+
 config.mongoURI = {
-    production: 'mongodb+srv://breezy_db_user:root@gallery-app-cluster.kt98dgw.mongodb.net/darkroom?retryWrites=true&w=majority&appName=gallery-app-cluster',
-    development: 'mongodb+srv://breezy_db_user:root@gallery-app-cluster.kt98dgw.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=gallery-app-cluster',
-    test: 'mongodb+srv://breezy_db_user:root@gallery-app-cluster.kt98dgw.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=gallery-app-cluster',
+    production: `mongodb+srv://${username}:${password}@${cluster}/darkroom?retryWrites=true&w=majority&appName=gallery-app-cluster`,
+    development: `mongodb+srv://${username}:${password}@${cluster}/darkroom-dev?retryWrites=true&w=majority&appName=gallery-app-cluster`,
+    test: `mongodb+srv://${username}:${password}@${cluster}/darkroom-test?retryWrites=true&w=majority&appName=gallery-app-cluster`,
 }
 module.exports = config;
